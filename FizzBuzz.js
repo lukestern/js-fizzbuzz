eba7ddd26426bcf18e501adf5fa9e797edca20d7
Luke Stern <luke.stern@softwire.com>
2021-05-24 14:41:14 +0100

    Rules for 7

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,14 +1,21 @@
 var i;
 var n = 100;
 for (i = 1; i <= n; i++) {
-    var result;
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
+    let result = '';
+
+    if (i % 3 === 0) {
+        result = 'fizz'
+    }
+    if (i % 5 === 0) {
+        result += 'buzz'
+    }
+    if (i % 7 === 0) {
+        result += 'bang';
+    }
+
+    if (result === '') {
         console.log(i);
+    } else {
+        console.log(result);
     }
 }
