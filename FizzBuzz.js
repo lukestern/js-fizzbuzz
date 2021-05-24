56c4dcc3f52301ce04020367f5663cfbf4d9745f
Luke Stern <luke.stern@softwire.com>
2021-05-24 17:22:49 +0100

    Rule re-work

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,36 +1,24 @@
-var i;
-var n = 255;
-for (i = 0; i <= n; i++) {
+let n = 250;
+for (let i = 0; i <= n; i++) {
     let result = [];
 
     if (i % 3 === 0) {
-        result.push('fizz');
-    }
-    if (i % 5 === 0) {
-        result.push('buzz');
-    }
-    if (i % 7 === 0) {
-        result.push('bang');
-    }
-    if (i % 11 == 0) {
-        result = ['bong'];
+        result.push('Fizz');
     }
     if (i % 13 === 0) {
-        let checkAnElementStartsWithB = (result.filter((element) => element.startsWith('b').length)) !== 0
-        if (result === '') {
-            result.push('fezz');
-        } else if (checkAnElementStartsWithB) {
-            let itemB = result.find(element => element[0] === 'b');
-            let indexB = result.indexOf(itemB)
-            result.splice(indexB, 0, 'fezz');
-        } else {
-            result.push('fezz');
-        }
+        result.push('Fezz')
+    }
+    if (i % 5 === 0) {
+        result.push('Buzz');
+    }
+    if (i % 7 === 0) {
+        result.push('Bang');
+    }
+    if (i % 11 === 0) {
+        result = ['Bong'];
     }
     if (i % 17 === 0) {
-        if (result !== '') {
-            result.reverse();
-        }
+        result.reverse();
     }
 
     if (result.length === 0) {
