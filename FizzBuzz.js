6e073c063f97e0ca167bf1faf99c9ce0ce199bd9
Luke Stern <luke.stern@softwire.com>
2021-05-24 15:22:57 +0100

    Rules for 13 and 17

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,25 +1,41 @@
 var i;
-var n = 100;
-for (i = 1; i <= n; i++) {
-    let result = '';
+var n = 255;
+for (i = 0; i <= n; i++) {
+    let result = [];
 
     if (i % 3 === 0) {
-        result = 'fizz'
+        result.push('fizz');
     }
     if (i % 5 === 0) {
-        result += 'buzz'
+        result.push('buzz');
     }
     if (i % 7 === 0) {
-        result += 'bang';
+        result.push('bang');
     }
-
     if (i % 11 == 0) {
-        result = 'bong';
+        result = ['bong'];
+    }
+    if (i % 13 === 0) {
+        let checkAnElementStartsWithB = (result.filter((element) => element.startsWith('b').length)) !== 0
+        if (result === '') {
+            result.push('fezz');
+        } else if (checkAnElementStartsWithB) {
+            let itemB = result.find(element => element[0] === 'b');
+            let indexB = result.indexOf(itemB)
+            result.splice(indexB, 0, 'fezz');
+        } else {
+            result.push('fezz');
+        }
+    }
+    if (i % 17 === 0) {
+        if (result !== '') {
+            result.reverse();
+        }
     }
 
-    if (result === '') {
+    if (result.length === 0) {
         console.log(i);
     } else {
-        console.log(result);
+        console.log(result.join(''));
     }
 }
